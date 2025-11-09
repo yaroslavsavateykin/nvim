7c96af71ad60ccf8dfc8415293e0d51d508a4713
yaroslav <yaroslavsavateikin@yandex.ru>
2025-11-09 23:27:20 +0300

    venv select update

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,5 +1,5 @@
 return {
-  {
+    {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
@@ -8,7 +8,7 @@ return {
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
+    --branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
